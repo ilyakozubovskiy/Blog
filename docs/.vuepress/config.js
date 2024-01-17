97f160d820186f028dc1fb86a1310f7d32efd533
Ilya Kazubouski <ikazubouski@ametros.com>
2024-01-17 17:16:22 +0300

    Added BoilerPlate

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,75 @@
 module.exports = {
-    title: "Ilya Kozubovskiy",
-    descciption: "ScienceSoft",
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Blog', link: '/blog/' },
-            { text: 'About me', link: '/about-me/' },
-            { text: 'GitHub', link: 'https://github.com/ilyakozubovskiy/' }
-        ],
-        // sidebar: 'auto'
+  title: 'Ilya Kozubovskiy',
+  base: '/',
+  description: 'Microsft',
+  theme: require.resolve('../../'),
+  themeConfig: {
+  authors: [
+      {
+        name: 'Ilya Kozubovskiy',
+        avatar: '/assets/img/avatar.png',
+        link: 'https://www.linkedin.com/in/ilyakozubovskiy/',
+        linktext: 'Follow',
+      },
+    ],
+    footer: {
+      contact: [
+        {
+          type: 'codepen',
+          link: '#',
+        },
+        {
+          type: 'facebook',
+          link: '#',
+        },
+        {
+          type: 'github',
+          link: 'https://github.com/ilyakozubovskiy',
+        },
+        {
+          type: 'gitlab',
+          link: '#',
+        },
+        {
+          type: 'instagram',
+          link: 'https://www.instagram.com/',
+        },
+        {
+          type: 'linkedin',
+          link: 'https://github.com/ilyakozubovskiy',
+        },
+        {
+          type: 'mail',
+          link: '#',
+        },
+        {
+          type: 'messenger',
+          link: '#',
+        },
+        {
+          type: 'phone',
+          link: '#',
+        },
+        {
+          type: 'twitter',
+          link: '#',
+        },
+        {
+          type: 'web',
+          link: '#',
+        }
+      ],
+      copyright: [
+        {
+          text: 'Licensed MIT.',
+          link: '#',
+        },
+      ],
     },
-    base: '/',
-    plugins: [
-        ['@vuepress/search', {
-          searchMaxSuggestions: 10
-        }]
-      ]
+
+    sitemap: {
+      hostname: 'https://github.com/ilyakozubovskiy'
+    },
+    smoothScroll: true
+  },
 }
